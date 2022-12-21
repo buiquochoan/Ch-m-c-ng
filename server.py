823bad9e502593985264965bf1997be899347fa8
Hoàn Bùi Quốc <hoan06031995@gmail.com>
2022-12-21 10:50:16 +0700

    get attendance realtime

--- server.py
+++ server.py
@@ -15,14 +15,17 @@ def makeDataAttendance(attendance):
     return {'office': HEAD_OFFICE, 'fingerprint_machine_id': attendance.user_id, 'datetime_string': attendance.timestamp.strftime("%Y-%m-%d %H:%M:%S")}
 
 def send_users(conn):
+    print('start send_users')
     url = format(os.getenv('APP_API'))+'/okr/store-multiple-users'
     results = []
     a_users = conn.get_users()
     for user in a_users:
         results.append({'office': HEAD_OFFICE, 'fingerprint_machine_id': user.uid, 'name': user.name})
     x = requests.post(url, json = results)
+    print ("send_users : {}".format(x))
 
 def send_attendances(conn, date_str = None):
+    print('start send_attendances')
     url = format(os.getenv('APP_API'))+'/okr/store-multiple-attendance'
     results = []
     date_check = date_str
@@ -30,9 +33,11 @@ def send_attendances(conn, date_str = None):
         date_check = datetime.datetime.now().strftime("%Y-%m-%d")
     attendances = conn.get_attendance()
     for attendance in attendances:
-        if attendance.timestamp.strftime("%Y-%m-%d") == date_check:
-            results.append(makeDataAttendance(attendance))
+        #if attendance.timestamp.strftime("%Y-%m-%d") == date_check:
+        results.append(makeDataAttendance(attendance))
+    print(results)
     x = requests.post(url, json = results)
+    print ("send_attendances : {}".format(x))
 
 def send_realtime_attendance(attendance):
     url = format(os.getenv('APP_API'))+'/okr/store-attendance'
@@ -42,7 +47,9 @@ def send_realtime_attendance(attendance):
 def start_connect(conn):
     send_users(conn)
     send_attendances(conn)
+
 def run_schedule(conn):
+    print('run schedule')
     schedule.every().day.at("12:00").do(send_users, conn)
     schedule.every().day.at("18:00").do(send_users, conn)
     schedule.every().day.at("23:30").do(send_attendances, conn)
@@ -51,6 +58,7 @@ def run_schedule(conn):
         time.sleep(1)
 
 def get_attendance_realtime(conn):
+    print('start live_capture')
     for attendance in conn.live_capture():
         if attendance is None:
           # implement here timeout logic
@@ -62,11 +70,10 @@ try:
     # connect to device
     conn = zk.connect()
     start_connect(conn)
-    run_schedule(conn)
+    #run_schedule(conn)
     get_attendance_realtime(conn)
 except Exception as e:
     print ("Process terminate : {}".format(e))
 finally:
     if conn:
         conn.disconnect()
-
